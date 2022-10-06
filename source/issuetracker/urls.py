7d7858b785fb95d015174823e0dace94ea8333dd
Yugai Denis <yugai_denis@mail.ru>
2022-10-06 15:45:47 +0600

    Создание шаблона удаления и редактирования, а также классов представления для редактирования, удаления и подтверждения удаления

--- source/issuetracker/urls.py
+++ source/issuetracker/urls.py
@@ -1,10 +1,13 @@
 ﻿from django.urls import path
 from issuetracker.views.base import IndexView
-from issuetracker.views.issues import IssueView, IssueAddView
+from issuetracker.views.issues import IssueView, IssueAddView, IssueUpdateView, IssueDelView, IssueDelConfirmView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('issue/<int:pk>', IssueView.as_view(), name='issue'),
-    path('issue_add/', IssueAddView.as_view(), name='issue_add')
+    path('issue_add/', IssueAddView.as_view(), name='issue_add'),
+    path('issue_update/<int:pk>', IssueUpdateView.as_view(), name='issue_update'),
+    path('issue_del/<int:pk>', IssueDelView.as_view(), name='issue_del'),
+    path('issue_del_confirm/<int:pk>', IssueDelConfirmView.as_view(), name='del_confirm')
 ]
--- source/issuetracker/views/issues.py
+++ source/issuetracker/views/issues.py
@@ -25,3 +25,41 @@ class IssueAddView(View):
             return render(request, 'issue_add.html', context={'form': form})
         issue = Issue.objects.create(**form.cleaned_data)
         return redirect('issue', pk=issue.pk)
+
+
+class IssueUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs['pk'])
+        form = IssueForm(initial={
+            'title': issue.title,
+            'description': issue.description,
+            'status': issue.status,
+            'type_issue': issue.type_issue
+        })
+        return render(request, 'issue_update.html', context={'form': form, 'issue': issue})
+
+    def post(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs['pk'])
+        form = IssueForm(data=request.POST)
+        if form.is_valid():
+            issue.title = form.cleaned_data['title']
+            issue.description = form.cleaned_data['description']
+            issue.status = form.cleaned_data['status']
+            issue.type_issue = form.cleaned_data['type_issue']
+            issue.save()
+            return redirect('issue', pk=issue.pk)
+        else:
+            return render(request, 'issue_update.html', context={'form': form, 'issue': issue})
+
+
+class IssueDelView(View):
+    def get(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs['pk'])
+        return render(request, 'issue_del.html', context={'issue': issue})
+
+
+class IssueDelConfirmView(View):
+    def post(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs['pk'])
+        issue.delete()
+        return redirect('index')
