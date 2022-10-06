e0f1eec770bd861a307f8f6260321d4b94836c74
Yugai Denis <yugai_denis@mail.ru>
2022-10-06 12:16:12 +0600

    Создание моделей по тз и описание их в админке + создание суперпользователя + создание миграции и применение её + выгрузка фикстуры

--- source/issuetracker/admin.py
+++ source/issuetracker/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from issuetracker.models import Issue, StatusIssue, TypeIssue
 
-# Register your models here.
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'status_id', 'type_issue_id', 'created_at')
+    list_filter = ('id', 'title', 'status_id', 'type_issue_id', 'created_at')
+    search_fields = ('title', 'description', 'status_id', 'type_issue_id', 'created_at')
+    fields = ('title', 'description', 'status', 'type_issue')
+
+admin.site.register(Issue, IssueAdmin)
+
+
+class StatusIssueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    # list_filter = 'title'
+    # search_fields = 'title'
+    # fields = 'title'
+
+admin.site.register(StatusIssue, StatusIssueAdmin)
+
+
+class TypeIssueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id')
+    # list_filter = ('id', 'title')
+    # search_fields = ('id', 'title')
+    # fields = ('id', 'title')
+
+admin.site.register(TypeIssue, TypeIssueAdmin)
+
--- source/issuetracker/models.py
+++ source/issuetracker/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class StatusIssue(models.Model):
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=20,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+
+class TypeIssue(models.Model):
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=20,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+
+class Issue(models.Model):
+    title = models.CharField(
+        verbose_name='Краткое описание',
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    description = models.TextField(
+        verbose_name='Полное описание',
+        max_length=800,
+        default=''
+    )
+    status = models.ForeignKey(
+        to='issuetracker.StatusIssue',
+        related_name='statuses',
+        on_delete=models.RESTRICT,
+        verbose_name='Статус'
+    )
+    type_issue = models.ForeignKey(
+        to='issuetracker.TypeIssue',
+        related_name='types',
+        on_delete=models.RESTRICT,
+        verbose_name='Тип'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Время создания',
+    )
+    updateted_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Время обновления'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title}'
